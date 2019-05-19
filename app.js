b1259e656fe6f11a4c4985fbcdd9f8d804e89ad2
IDM.air <innanadm8@gmail.com>
2019-05-18 23:43:06 -0400

    adding loop, making board

--- app.js
+++ app.js
@@ -19,6 +19,30 @@
           };
         }
 
+        var state = initialState();
+        function renderBoard() {
+          DOM.$currentPlayer.textContent = state.players[state.currentPlayer];
+          // Assuming SIZE > 0.
+          DOM.$board.innerHTML = '';
+
+          for (let i = 0; i < SIZE; i++) {
+            const $row = document.createElement('div');
+            $row.classList.add('board-row');
+
+for (let j = 0; j < SIZE; j++) {
+              const $cell = document.createElement('div');
+              $cell.classList.add('board-cell');
+              $cell.setAttribute('data-i', i);
+              $cell.setAttribute('data-j', j);
+              const $content  = document.createElement('span');
+              $content.classList.add('content');
+              $content.textContent = state.boardModel[i][j];
+              $cell.appendChild($content);
+              $row.appendChild($cell);
+            }
+            DOM.$board.appendChild($row);
+          }
+        }
 
 
 
