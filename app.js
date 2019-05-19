e072a6e8c0f6378181f7abb28009d154f750da1f
IDM.air <innanadm8@gmail.com>
2019-05-18 23:38:11 -0400

    initializing gameboard

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 (() => {
     console.log("let's get it!");
 
+    function init () {
+        const DOM = {
+          $currentPlayer: document.querySelector('.js-current-player'),
+          $board: document.querySelector('.js-board'),
+          $resetButton: document.querySelector('.js-reset'),
+        };
+
+        const SIZE = 3;
+        function initialState() {
+          return {
+            boardModel: Array(SIZE).fill(null).map(_ => Array(SIZE).fill(null)),
+            players: ['X', 'O'],
+            currentPlayer: 0,
+            gameEnded: false,
+            turn: 0,
+          };
+        }
 
 
 
